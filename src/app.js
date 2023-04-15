bb3e139d4617d809273e699a741384ffb698803f
Phillip Cofield <plcofield@gmail.com>
2023-04-15 01:05:18 +0000

    your message here

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
 };
+
+let generateExcuse = () => {
+let pronoun = ["a", "the", "that"];
+let subject = ["car","Jill","Billy","Cat"];
+let action = [ "ran","skip","hop","dance"];
+let item = ["pillow","jacket","hat","shoe"];
+let place = ["the sun","the moon","the star"];
+
+let proIndex = Math.floor(Math.random()) * pronoun.length;
+let subjectIndex = Math.floor(Math.random()) * subject.length;
+let actionIndex= Math.floor(Math.random()) * action.length;
+let itemIndex = Math.floor(Math.random()) * item.length;
+let placeIndex = Math.floor(Math.random()) * place.length;
+
+return(
+  pronoun[proIndex] + " " +
+  subject[subjectIndex] + " " +
+  action[actionIndex] + " " +
+  item[itemIndex] + " " +
+  place[placeIndex]
+
+)
+
+}
